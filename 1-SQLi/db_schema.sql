8dfbd8faffe44d410adf76d91386c9ef24451cbc
Vincent Llorens <zakorahq@yahoo.fr>
2013-06-20 22:09:56 +0200

    Change sqli db name to prism_secrets

--- 1-SQLi/db_schema.sql
+++ 1-SQLi/db_schema.sql
@@ -1,8 +1,8 @@
-drop table if exists prism_people;
-create table prism_people (
+drop table if exists prism_secrets;
+create table prism_secrets (
   id integer primary key autoincrement,
   name text not null,
   secret text not null
 );
 
-insert into prism_people values (null, "Obama", "PRISM");
+insert into prism_secrets values (null, "Obama", "PRISM");
