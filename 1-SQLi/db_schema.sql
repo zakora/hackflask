6d9f2210a06bebcd3021cc5f30cd4d33a7f98c16
Vincent Llorens <zakorahq@yahoo.fr>
2013-06-22 13:03:18 +0200

    Update SQLi database population

--- 1-SQLi/db_schema.sql
+++ 1-SQLi/db_schema.sql
@@ -5,4 +5,9 @@ create table prism_secrets (
   secret text not null
 );
 
+-- Insert random name/secret in the database
 insert into prism_secrets values (null, "Obama", "PRISM");
+insert into prism_secrets values (null, "George W Bush", "petroleum");
+insert into prism_secrets values (null, "Angela Merkel", "We want our own prism");
+insert into prism_secrets values (null, "Dingo", "not a dog");
+insert into prism_secrets values (null, "Superman", "cry when he sees a green rock");
